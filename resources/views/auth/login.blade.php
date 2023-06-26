afe3dc77da2d929131246ded0ac90bdecd8bf1c5
Hening12345 <hening.rifqi@Gmail.com.com>
2023-06-26 20:22:24 +0700

    komit 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,11 +41,11 @@
                             <a href="{{ route('register') }}" class="link footer-link">{{ __('Create Account') }}</a>
                         </h6>
                     </div>
-                    <div class="pull-right">
+                    <!--<div class="pull-right">
                         <h6>
                             <a href="{{ route('password.request') }}" class="link footer-link">{{ __('Forgot password?') }}</a>
                         </h6>
-                    </div>
+                    </div>-->
                 </div>
             </div>
         </form>
